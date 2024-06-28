6a5cdd4d70b059ec17e5bcb65cef11de13e8adfd
jung won hyeong <jeongwon264@gmail.com>
2024-06-28 15:23:43 +0900

    add [정원형]: 이용자가 선택한 여행지를 보여줍니다. [Vue-TeamProject-10]
    이미지 및 가격 추가 표기 및 페이지 ui변경

--- potato/src/travel/pages/read/TravelReadPage.vue
+++ potato/src/travel/pages/read/TravelReadPage.vue
@@ -1,56 +1,54 @@
 <template>
-    <v-container>
-        <h2>게시물 읽기!</h2>
-        <v-card v-if="travel">
-            <v-card-title>게시물 정보</v-card-title>
-            <v-card-text>
-                <v-container>
-                    <v-row>
-                        <v-col cols="12">
-                            <v-text-field v-model="travel.travelName" readonly label="제목"/>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="12">
-                            <v-text-field v-model="travel.travelLocation" readonly label="작성자"/>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-         /               <v-col cols="12">
-                            <v-textarea v-model="travel.travelContent" readonly label="내용" auto-grow/>
-                        </v-col>
-                    </v-row>
-                    <!-- <v-row>
-                        <v-col cols="12">
-                            <v-img :src="getReviewImageUrl(travel.travelImage)" aspect-ratio="1" class="grey lighten-2">
-                                <template v-slot:placeholder>
-                                    <v-row class="fill-height ma-0" align="center" justify="center">
-                                        <v-progress-circular indeterminate color="grey lighten-5"/>
-                                    </v-row>
-                                </template>
-                            </v-img>
-                        </v-col>
-                    </v-row> -->
-                    <!-- <v-row justify="end">
-                        <v-col cols="auto">
-                            <router-link :to="{ name: 'TravelBoardModifyPage', params: { BoardId } }">
-                                <v-btn color="primary">수정</v-btn>
-                            </router-link>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-btn color="error" @click="onDelete">삭제</v-btn>
-                        </v-col>
-                        <v-col cols="auto">
-                            <router-link :to="{ name: 'TravelBoardListPage' }">
-                                <v-btn color="secondary">돌아가기</v-btn>
-                            </router-link>
-                        </v-col>
-                    </v-row> -->
-                </v-container>
-            </v-card-text>
-        </v-card>
-    </v-container>
+  <v-container>
+    <h2 class="text-center mb-5">여행지 상세정보</h2>
+    <v-card v-if="travel" class="mx-auto" max-width="800" outlined>
+      <!-- 이미지 컴포넌트 -->
+      <v-img :src="getReviewImageUrl(travel.travelImage)" height="400px" class="white--text align-end" gradient="to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.5)">
+      </v-img>
+      <!-- 여행지 이름을 이미지 아래에 위치 -->
+      <v-card-title class="display-1 py-4">{{ travel.travelName }}</v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="travel.travelPrice" readonly label="가격" outlined dense></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="travel.travelLocation" readonly label="카테고리" outlined dense></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-textarea v-model="travel.travelProperty" readonly label="특성" auto-grow outlined dense></v-textarea>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-textarea v-model="travel.travelContent" readonly label="여행 리뷰" auto-grow outlined dense></v-textarea>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+.mb-5 {
+  margin-bottom: 3rem;
+}
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
+}
+.py-4 {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+</style>
 
 <script>
 import { mapActions, mapState } from 'vuex'
@@ -70,10 +68,10 @@ export default {
     // 삭제 구현 by 아람
     methods: {
         ...mapActions(travelModule, ['requestTravelToDjango']),
-        // getReviewImageUrl (reviewImage) {
-        //     console.log('reviewImage:', reviewImage)
-        //     return require('@/assets/images/uploadImages/' + reviewImage)
-        // },
+        getReviewImageUrl (travelImage) {
+            console.log('travelImage:', travelImage)
+            return require('@/assets/images/uploadImages/' + travelImage)
+        },
         // async onDelete() {
         //     console.log('삭제를 누르셨습니다!')
         //     await this.requestDeleteTravelBoardToDjango(this.BoardId)
