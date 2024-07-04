48f28220737c8389cf54d9af1c105c68c288d32a
lleehs <lhs3789@gmail.com>
2024-07-04 21:34:36 +0900

    add: [이현석] 사용자가 로그인 버튼을 누르면 카카오 계정으로 로그인 할 수 있습니다. [Vue-TeamProject-11]
    이용자가 로그인하는 페이지의 ui를 개선했습니다.

--- potato/src/account/pages/login/AccountLoginPage.vue
+++ potato/src/account/pages/login/AccountLoginPage.vue
@@ -1,14 +1,29 @@
 <template>
-    <v-container>
-        <v-card>
-            <v-img class="mx auto" height="300" 
-                :src="require('@/assets/images/fixed/말감.png')"/>
-        </v-card>
+  <v-container class="login-container" fluid>
+    <v-row align="center" justify="center" class="fill-height">
+      <v-col cols="12" sm="8" md="6" lg="4">
+        <v-card class="login-card" elevation="8">
+          <v-img
+            class="mx-auto"
+            height="500"
+            :src="require('@/assets/images/fixed/로그인.png')"
+          >
+            <v-card-title class="text-h4 white--text">
+            
+            </v-card-title>
+          </v-img>
+          
+          <v-card-text class="text-center pt-6">
+            <h2 class="text-h5 mb-4 custom-font">감자가 되고 싶다고?⬇️⬇️</h2>
             <v-img class="icon"
-                    :src="require('@/assets/images/fixed/kakao_login.png')"
-                    height="160"
+                    :src="require('@/assets/images/fixed/kakao_login2.png')"
+                    height="100"
                     @click="goToKakaoLogin"/>
-    </v-container>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -28,10 +43,38 @@ export default {
 }</script>
 
 <style scoped>
-.login_icons {
-    margin-top: 20px;
+
+@import url('https://fonts.googleapis.com/css2?family=Gaegu:wght@700&display=swap');
+
+.login-container {
+  background-image: url('@/assets/images/fixed/LB4.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
 }
-.icons{
-    cursor: pointer;
+
+.login-card {
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 16px;
+  overflow: hidden;
+}
+
+.icon {
+  cursor: pointer;
+  transition: transform 0.3s ease;
+}
+
+.icon:hover {
+  transform: scale(1.05);
+}
+
+.custom-font {
+  font-family: 'Gaegu', cursive;
+  font-size: 2.5rem;
+  color: black;
+  text-shadow: 2px 2px 4px rgba(0,0,0,0.1);
 }
 </style>
